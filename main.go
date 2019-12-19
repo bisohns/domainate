65f33178396a096fc6123afdcc69448e1cc3cba3
Diretnan Domnan <diretnandomnan@gmail.com>
2019-12-19 05:07:11 +0100

    WIP: Generating synonyms from BigAPI

--- main.go
+++ main.go
@@ -3,17 +3,31 @@ package main
 import (
 	"flag"
 	"github.com/bisoncorps/domainate/pkg/generate"
+	"github.com/bisoncorps/domainate/pkg/thesaurus"
 	"github.com/deven96/gosock/pkg/custlog"
 )
 
 // LogFile : global logfile to be used
 var LogFile = flag.String("log", "domainate.log", "Name of the log file to save to")
+var apikey = flag.String("apikey", "asdaksjdjfsjasdjasd", "API key of BigAPI")
 
 func main() {
 	flag.Parse() //parse the flags
 	//    get the default writers for log
 	defwriters := custlog.DefaultWriters(*LogFile, false)
+	word := "chow"
 	custlog.LogInit(defwriters)
 	custlog.Info.Println("Domainate started")
-	custlog.Info.Printf(`Example domain names using "bisoncorps" as keyword => %v`, generate.Domains("bisoncorps"))
+	custlog.Info.Printf(`Example domain names using %v as keyword => %v`, word, generate.Domains(word))
+	thesaurus := &thesaurus.BigHuge{APIKey: *apikey}
+	syns, err := thesaurus.Synonyms(word)
+	if err != nil {
+		custlog.Error.Println("Failed when looking for synonyms for" + word + " ")
+	}
+	if len(syns) == 0 {
+		custlog.Error.Println("Couldn't find any synonyms for" + word + " ")
+	}
+	if len(syns) > 0 {
+		custlog.Info.Printf(`Synonyms found are %v`, syns)
+	}
 }
---
+++ pkg/thesaurus/thesaurus.go
@@ -0,0 +1,54 @@
+package thesaurus
+
+import (
+	"encoding/json"
+	"errors"
+	"net/http"
+)
+
+// BigHuge : API handler, initialize with API key
+type BigHuge struct {
+	APIKey string
+}
+
+type synonyms struct {
+	Noun *words `json:"noun"`
+	Verb *words `json:"verb"`
+}
+
+type words struct {
+	Syn []string `json:"syn"`
+}
+
+// Synonyms : get the synonyms of any word
+func (b *BigHuge) Synonyms(term string) ([]string, error) {
+	var syns []string
+	response, err := http.Get("http://words.bighugelabs.com/api/2/" + b.APIKey + "/" + term + "/json")
+
+	if err != nil {
+		return syns, errors.New(`bighuge: Failed when looking for synonyms for ` + term + `` + err.Error())
+	}
+
+	var data synonyms
+	defer response.Body.Close()
+
+	if err := json.NewDecoder(response.Body).Decode(&data); err != nil {
+		return syns, err
+	}
+
+	if data.Noun != nil {
+		syns = append(syns, data.Noun.Syn...)
+	}
+
+	if data.Verb != nil {
+		syns = append(syns, data.Verb.Syn...)
+	}
+
+	return syns, nil
+
+}
+
+// Thesaurus : interface for any thesaurus API to extend
+type Thesaurus interface {
+	Synonyms(term string) ([]string, error)
+}
